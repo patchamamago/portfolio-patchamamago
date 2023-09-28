294c18b0f27ff7eae3c61945d6dbb4bb8ea85ab7
A. Urquiola <patchamama.go@gmail.com>
2023-09-28 12:09:42 +0200

    add fonts, styles and logo and change server script

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # Portafolio project
 
+# Run local NodeJs server to do tests
+
+```sh
+node webserver/server.js
+```
+
 # Credits
 
 - Bootstrap 
+- NodeJS (optional to test local develop)
--- webserver/server.js
+++ webserver/server.js
@@ -3,19 +3,41 @@ const fs = require('fs');
 const path = require('path');
 
 const server = http.createServer((req, res) => {
-    const filePath = path.join(__dirname, req.url === '/' ? '/../index.html' : req.url);
-    console.log(filePath);
+    // Get the file path
+    const filePath = path.join(__dirname, '..', req.url === '/' ? 'index.html' : req.url);
 
-    fs.readFile(filePath, (err, data) => {
+    // Check if the file exists
+    fs.access(filePath, fs.constants.F_OK, (err) => {
         if (err) {
             res.writeHead(404, { 'Content-Type': 'text/plain' });
             res.end('File not found');
         } else {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(data);
+            // Read and serve the file over response
+            fs.readFile(filePath, (err, data) => {
+                if (err) {
+                    res.writeHead(500, { 'Content-Type': 'text/plain' });
+                    res.end('Internal Server Error');
+                } else {
+                    const contentType = getContentType(filePath);
+                    res.writeHead(200, { 'Content-Type': contentType });
+                    res.end(data);
+                }
+            });
         }
     });
 });
+
+function getContentType(filePath) {
+    const extname = path.extname(filePath);
+    switch (extname) {
+        case '.html':
+            return 'text/html';
+        case '.css':
+            return 'text/css';
+        default:
+            return 'text/plain';
+    }
+}
 
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
